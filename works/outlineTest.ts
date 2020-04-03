23361b658731826afe6df3f7d80dd36e572fd068
A Freilich <freilich.am@gmail.com>
2020-04-02 23:22:28 -0400

    improve outline test

--- works/outlineTest.ts
+++ works/outlineTest.ts
@@ -1,4 +1,5 @@
 import { smoothDraw } from "../jpgUtilities/writeJpg";
 import { grid, outline } from "../colorTypes/colorFxn";
+import { griddyCon, composeThen } from "../convolutions/types/convolution";
 
-smoothDraw(outline(grid), 'outlineTest.jpg', 1200, 1200, 10);
+smoothDraw(outline(composeThen(griddyCon(), grid)), 'outlineTest.jpg', 1200, 1200, 10);
